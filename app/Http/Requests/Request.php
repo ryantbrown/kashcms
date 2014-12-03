006de8b3789606140e474574d9b1584cfee6954c
Ryan Brown <ryan@Ryans-MacBook-Pro.local>
2014-12-03 12:33:08 -0800

    kash for gold

---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,9 @@
+<?php namespace Kash\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest {
+
+	//
+
+}
---
+++ config/local/database.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+	'connections' => [
+
+		'mysql' => [
+			'host'      => 'localhost',
+			'database'  => 'kash',
+			'username'  => 'homestead',
+			'password'  => 'secret',
+		],
+
+	],
+
+];
